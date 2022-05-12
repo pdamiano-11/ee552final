5a3b7a51cef6c938fb8d1db797f4f20bb7847648
pdamiano-11 <pdamiano@stevens.edu>
2022-05-11 20:10:42 -0400

    fixed coordinates in UI

--- src/main/java/UserInterface/UserInterface.java
+++ src/main/java/UserInterface/UserInterface.java
@@ -23,15 +23,15 @@ public class UserInterface extends PApplet {
     boolean resultScreen = false;
 
     // these are temporary but should be sent in from the other file
-    int numAdj = 13;
-    int numNoun = 10;
-    int numVerb = 8;
-    int numPnoun = 15;
-    int numAdV = 6;
+    int numAdj = 2;
+    int numNoun = 2;
+    int numVerb = 2;
+    int numPnoun = 2;
+    int numAdV = 2;
 
     @Override
 	public void settings() {
-		size(900, 900);
+		size(800, 600);
 	}
 
     @Override
@@ -54,13 +54,13 @@ public class UserInterface extends PApplet {
         // ---------------------------------- Start Screen Logic
         // ----------------------------------------
         if (startScreen) {
-            image(logo, width / 4, height / 4, logo.width / 2, logo.height / 2);
-            image(genButton, (width / 4) + 23, height * 2 / 4, genButton.width / 2, genButton.height / 2);
+            image(logo, width / 5, height / 5, logo.width / 2, logo.height / 2);
+            image(genButton, (width / 5) + 25, height * 2/3, genButton.width / 2, genButton.height / 2);
 
             // generate button was| pressed
             if ((mousePressed)
-                    && ((mouseX < ((width / 4) + 23) + genButton.width / 2) && (mouseX > (width / 4) + 23))) {// checking if mouse is pressed and if x is in button range
-                if ((mouseY < ((height * 2 / 4) + genButton.height / 2)) && (mouseY > (height * 2 / 4))) { // checking if mouse y is in button range
+                    && ((mouseX < ((width / 5) + 25) + genButton.width / 2) && (mouseX > (width / 5) + 25))) {// checking if mouse is pressed and if x is in button range
+                if ((mouseY < ((height * 2 / 3) + genButton.height / 2)) && (mouseY > (height * 2 / 3))) { // checking if mouse y is in button range
                     startScreen = false;
                     inputScreen1 = true;
                 }
@@ -72,35 +72,35 @@ public class UserInterface extends PApplet {
             image(logo, 0, 0, logo.width * 3 / 8, logo.height * 3 / 8);
 
             // section header placement
+            textSize(20);
 
-            image(secHea, 20, 200, secHea.width / 2, secHea.height / 2);
-            textSize(30);
+            image(secHea, 15, 200, secHea.width * 2 / 5, secHea.height / 2);
             fill(3, 152, 158); // dark color
             text("Enter  " + numAdj + "  Adjectives", 55, 250);
             fill(130, 191, 194); // light color
             stroke(130, 191, 194);
-            rect(22, 263, 310, numAdj * 35);
+            rect(17, 265, 248, numAdj * 35);
 
-            image(secHea, 350, 200, secHea.width / 2, secHea.height / 2);
+            image(secHea, 275, 200, secHea.width * 2 / 5, secHea.height / 2);
             fill(3, 152, 158); // dark color
-            text("Enter  " + numNoun + "  Nouns", 400, 250);
+            text("Enter  " + numNoun + "  Nouns", 315, 250);
             fill(130, 191, 194); // light color
             stroke(130, 191, 194);
-            rect(352, 263, 310, numNoun * 35);
+            rect(277, 265, 248, numNoun * 35);
 
-            image(secHea, 680, 200, secHea.width / 2, secHea.height / 2);
+            image(secHea, 535, 200, secHea.width * 2 / 5, secHea.height / 2);
             fill(3, 152, 158); // dark color
-            text("Enter  " + numVerb + "  Verbs", 730, 250);
+            text("Enter  " + numVerb + "  Verbs", 575, 250);
             fill(130, 191, 194); // light color
             stroke(130, 191, 194);
-            rect(682, 263, 310, numVerb * 35);
+            rect(537, 265, 248, numVerb * 35);
 
             // next button
-            image(nextButt, (width / 3) + 40, (height * 4 / 5) + 130, nextButt.width / 3, nextButt.height / 3);
+            image(nextButt, (width / 3) + 40 , (height * 5/6) , nextButt.width / 4, nextButt.height / 4);
 
             // next button was pressed
             if ((mousePressed) && ((mouseX < ((width / 3) + 40) + nextButt.width / 3) && (mouseX > (width / 3) + 40))) { // checking if mouse is pressed and if x is in button range
-                if ((mouseY < ((height * 4 / 5) + 130 + nextButt.height / 3)) && (mouseY > ((height * 4 / 5) + 130))) { // checking if mouse y is in button range
+                if ((mouseY < ((height * 5 / 6) + nextButt.height / 3)) && (mouseY > ((height * 5 / 6)))) { // checking if mouse y is in button range
                     if (inputScreen1) {
                         inputScreen2 = true;
                         inputScreen1 = false;
@@ -114,29 +114,29 @@ public class UserInterface extends PApplet {
         if (inputScreen2) {
             image(logo, 0, 0, logo.width * 3 / 8, logo.height * 3 / 8);
             // section header
-            image(secHea, 135, 200, secHea.width / 2, secHea.height / 2);
+            image(secHea, 145, 200, secHea.width * 2 / 5, secHea.height / 2);
             fill(3, 152, 158); // dark color
-            text("Enter  " + numPnoun + "  Proper Nouns", 150, 250);
+            text("Enter  " + numPnoun + "  Proper Nouns", 165, 250);
             fill(130, 191, 194); // light color
             stroke(130, 191, 194);
-            rect(138, 263, 310, numPnoun * 35);
+            rect(147, 265, 248, numPnoun * 35);
 
-            image(secHea, 535, 200, secHea.width / 2, secHea.height / 2);
+            image(secHea, 405, 200, secHea.width * 2 / 5, secHea.height / 2);
             fill(3, 152, 158); // dark color
-            text("Enter  " + numAdV + "  Adverbs", 580, 250);
+            text("Enter  " + numAdV + "  Adverbs", 435, 250);
             fill(130, 191, 194); // light color
             stroke(130, 191, 194);
-            rect(538, 263, 310, numVerb * 35);
+            rect(407, 265, 248, numVerb * 35);
 
             // result button
-            image(resultButton, (width / 3) + 40, (height * 4 / 5) + 80, resultButton.width / 3,
+            image(resultButton, (width / 3), (height * 5 / 6) + 30, resultButton.width / 3,
                     resultButton.height / 3);
 
             // result button was pressed
             if ((mousePressed)
-                    && ((mouseX < ((width / 3) + 40) + resultButton.width / 3) && (mouseX > (width / 3) + 40))) { // checking if mouse is pressed and if x is in button range
-                if ((mouseY < ((height * 4 / 5) + 80 + resultButton.height / 3))
-                        && (mouseY > ((height * 4 / 5) + 80))) { // checking if mouse y is in button range
+                    && ((mouseX < ((width / 3)) + resultButton.width / 3) && (mouseX > (width / 3)))) { // checking if mouse is pressed and if x is in button range
+                if ((mouseY < ((height * 5 / 6) + 30 + resultButton.height / 3))
+                        && (mouseY > ((height * 5 / 6) + 30))) { // checking if mouse y is in button range
                     if (inputScreen2) {
                         inputScreen2 = false;
                         resultScreen = true;
@@ -150,7 +150,7 @@ public class UserInterface extends PApplet {
             image(logo, 0, 0, logo.width * 3 / 8, logo.height * 3 / 8);
             image(resetButt, width - 100, 60, resetButt.width / 2, resetButt.height / 2);
             image(openFile, width - 300, 65, openFile.width / 2, openFile.height / 2);
-            rect(40, 200, 900, 700); // This is the area to display the text from the filled in story
+            rect(40, 200, 700, 350); // This is the area to display the text from the filled in story
 
             // reset button pressed
             if ((mousePressed) && ((mouseX < ((width - 100)) + resetButt.width / 2) && (mouseX > (width - 100)))) { // checking if mouse is pressed and if x is in button range
